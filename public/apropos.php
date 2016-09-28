43edb5485d54187404d34a8fb867e936fef52ba1
dbeauseigneur <dbeauseigneur@gmail.com>
2016-09-28 14:01:35 +0200

    correction répertoire et footer

--- public/apropos.php
+++ public/apropos.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" name="viewport">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>La Folle Agence</title>
 </head>
 
@@ -28,12 +28,6 @@
 ?>
 
 
-<footer class="container-fluid">
-    <?php
-    include('../src/footer.php');
-    ?>
-</footer>
-
-</body>
-
-</html>
+<?php
+include('../src/footer.php');
+?>
--- public/articleblog.php
+++ public/articleblog.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" name="viewport">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>La Folle Agence</title>
 </head>
 
@@ -28,14 +28,8 @@
 ?>
 
 
-<footer class="container-fluid">
-    <?php
-    include('../src/footer.php');
-    ?>
-</footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-</body>
+<?php
+include('../src/footer.php');
+?>
 
-</html>
