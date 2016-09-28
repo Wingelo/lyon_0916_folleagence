c911fd457a8c6f59c0751a4232e5f970d99eb83b
dbeauseigneur <dbeauseigneur@gmail.com>
2016-09-28 11:22:35 +0200

    ajout squelette

---
+++ public/contact.php
@@ -0,0 +1,35 @@
+<head>
+    <meta charset="utf-8" name="viewport">
+    <link rel="stylesheet" href="style.css">
+    <title>La Folle Agence</title>
+</head>
+
+<body>
+
+<!-- Entête du site -->
+<header>
+    <!-- Menu de navigation -->
+    <?php
+    include ('../src/navbar.php') ;
+    ?>
+</header>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 28/09/16
+ * Time: 10:01
+ */
+?>
+
+
+<footer class="container-fluid">
+    <?php
+    include ('../src/footer.php') ;
+    ?>
+</footer>
+
+</body>
+
+</html>
---
+++ src/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 28/09/16
+ * Time: 09:47
+ */
