2ebdf92127f8505826ba09c4ebad9ae4eadcfcd5
Wingelo <cedric.gamrat@gmail.com>
2016-09-30 09:53:34 +0200

    Navbar + Public

--- public/articleblog.php
+++ public/articleblog.php
@@ -6,6 +6,7 @@
                   integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
             <link href="css/blog-post.css" rel="stylesheet">
             <link rel="stylesheet" href="css/style.css">
+            <link rel="stylesheet" type="text/css" href="css/navbar.css">
             <title>La Folle Agence</title>
         </head>
 
