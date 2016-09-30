d5cdcffe2a591448c1d76d293c457c4c404bb378
Wingelo <cedric.gamrat@gmail.com>
2016-09-30 08:24:54 +0200

    fin de la nav bar

---
+++ src/navbar.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <!-- Style font -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500" rel="stylesheet">
+    <!-- CSS bootstrap -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- CSS navbar.css -->
+    <link rel="stylesheet" type="text/css" href="../public/css/navbar.css">
+    <!-- Done -->
+    <meta charset="UTF-8">
+
+</head>
+<body>
+<header>
+    <div>
+        <!-- Menu nav bar -->
+        <div id="custom-bootstrap-menu" class="navbar navbar-default navbar-fixed-top " role="navigation">
+            <div class="container-fluid">
+                <div class="container">
+                    <div class="navbar-header"><a class="navbar-brand" href="#"><img id='logo_ptt'
+                                                                                     src="../public/images/logo-lafolleagence-web.png"/></a>
+                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                data-target=".navbar-menubuilder"><span class="sr-only">Toggle navigation</span><span
+                                class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse navbar-menubuilder">
+                        <ul class="nav navbar-nav navbar-right icone-nav">
+                            <li><a href="http://www.justine-remaudiere.com/"><img src="../public/images/persona.png"></a></li>
+                            <li><a href="https://www.facebook.com/LaFolleAgence"><img src="../public/images/facebook.png"></a></li>
+                            <li><a href="https://www.instagram.com/lafolleagence/"><img src="../public/images/twitter.png"></a></li>
+                            <li><a href="https://twitter.com/lafolleagence"><img src="../public/images/instagram.png"></a></li>
+                            <li><a href="https://plus.google.com/+Lafolleagence"><img src="../public/images/googleplus.png"></a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="../public/apropos.php">A propos</a></li>
+                            <li><a href="../public/prestations.php">Prestations</a></li>
+                            <li><a href="../public/refproj.php">Référence & projets</a></li>
+                            <li><a href="../public/blog.php">Blog</a></li>
+                            <li><a href="../public/contact.php">Contact</a></li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+</header>
+
+
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</html>
