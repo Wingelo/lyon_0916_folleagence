93e255a3f087fc6bc1ed5c336757241bdb35eb58
Axelle-slv <axelle.silva@gmail.com>
2016-09-30 16:26:40 +0200

    Modification de la navbar et notamment des positions des elements et des liens aux reseaux sociaux

--- public/index.php
+++ public/index.php
@@ -5,137 +5,136 @@
     <meta charset="utf-8" name="viewport">
     <link href="https://fonts.googleapis.com/css?family=Bad+Script" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" type="text/css" href="css/navbar.css">
-    <title>La Folle Agence</title>
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
+    <script src="https://use.fontawesome.com/d97479808e.js"></script>
+    <title>La Folle Agence | Votre communication numérique autrement !</title>
 </head>
 
+<body>
 
-        <body>
-        <!-- Entête du site -->
-        <header>
-            <!-- Menu de navigation -->
-            <?php include('../src/navbar.php'); ?>
-        </header>
+    <header>
+        <!-- Menu de navigation -->
+        <?php include('../src/nav.php'); ?>
+    </header>
 
-            <section class="container-fluid">
-                <div class="container">
-                    <div class="image_acceuil">
-                        <img src="">
+        <section class="container-fluid">
+            <div class="container">
+                <div class="image_accueil">
+                    <img src="">
+                </div>
+                <div class="row">
+                    <div class="">
+                        <h1>La Folle Agence</h1>
                     </div>
-                    <div class="row">
-                        <div class="">
-                            <h1>La Folle Agence</h1>
-                        </div>
-                        <div class="">
-                            <h2>Votre communication numérique autrement!</h2>
-                        </div>
+                    <div class="">
+                        <h2>Votre communication numérique autrement!</h2>
                     </div>
-                    <div class="row">
-                        <div class="">
-                            <button class="cliquez"></button>
-                        </div>
+                </div>
+                <div class="row">
+                    <div class="">
+                        <button class="cliquez"></button>
                     </div>
-                    <div class="row">
-                        <div class="">
-                            <button class="scroll_bottom"></button>
+                </div>
+                <div class="row">
+                    <div class="">
+                        <button class="scroll_bottom"></button>
+                    </div>
+                </div>
+                <div class="">
+                    <button class="scroll_top"></button>
+                </div>
+            </div>
+        </section>
+
+        <section class="bandeau container-fluid"></section>
+
+        <section class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="">
+                        <h3>Qui se cache derrière la Folle Agence ?</h3>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="">
+                        <div class="image_portrait">
+                            <img src="">
                         </div>
                     </div>
                     <div class="">
-                        <button class="scroll_top"></button>
+                        <p>texte</p>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
 
-            <section class="bandeau container-fluid"></section>
-
-            <section class="container-fluid">
-                <div class="container">
-                    <div class="row">
-                        <div class="">
-                            <h3>Qui se cache derrière la Folle Agence ?</h3>
-                            <hr>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="">
-                            <div class="image_portrait">
-                                <img src="">
-                            </div>
-                        </div>
-                        <div class="">
-                            <p>texte</p>
-                        </div>
+        <section class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="">
+                        <h3>Les prestations proposées</h3>
+                        <hr>
                     </div>
                 </div>
-            </section>
+                <div class="row">
+                    <div class="">
+                        <div class="mdpe">
 
-            <section class="container-fluid">
-                <div class="container">
-                    <div class="row">
-                        <div class="">
-                            <h3>Les prestations proposées</h3>
-                            <hr>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="">
-                            <div class="mdpe">
+                    <div class="">
+                        <div class="cm">
 
-                            </div>
                         </div>
-                        <div class="">
-                            <div class="cm">
+                    </div>
+                    <div class="">
+                        <div class="vc">
 
-                            </div>
-                        </div>
-                        <div class="">
-                            <div class="vc">
-
-                            </div>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
 
-            <section class="bandeau container-fluid"></section>
+        <section class="bandeau container-fluid"></section>
 
-            <section class="container-fluid">
-                <div class="container">
-                    <div class="row">
-                        <div class="">
-                            <h3>Discutons de vos projets autour d'un café ?</h3>
-                        </div>
-                        <div class="">
-                            <button class="contacter"></button>
-                        </div>
+        <section class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="">
+                        <h3>Discutons de vos projets autour d'un café ?</h3>
+                    </div>
+                    <div class="">
+                        <button class="contacter"></button>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
 
-            <section class="container-fluid">
-                <div class="container">
-                    <div class="row">
-                        <div class="">
-                            <h3>Les derniers articles du Blog</h3>
-                            <hr>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="">
-                            <div class="carousel"></div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="">
-                            <button class="blog"></button>
-                        </div>
+        <section class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="">
+                        <h3>Les derniers articles du Blog</h3>
+                        <hr>
                     </div>
                 </div>
-            </section>
+                <div class="row">
+                    <div class="">
+                        <div class="carousel"></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="">
+                        <button class="blog"></button>
+                    </div>
+                </div>
+            </div>
+        </section>
 
 
 
@@ -143,5 +142,5 @@
 
 
 
-        <?php include('../src/footer.php'); ?>
+    <?php include('../src/footer.php'); ?>
 
---
+++ src/nav.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+            <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-menubuilder" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">
+                        <img alt="La Folle Agence | Votre communication digitale autrement" src="../public/images/logo-lafolleagence-web.png">
+                    </a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-right" id="navbar-menubuilder">
+                    <ul class="nav navbar-nav">
+                        <li><a href="../public/apropos.php">A propos</a></li>
+                        <li><a href="../public/prestations.php">Prestations</a></li>
+                        <li><a href="../public/refproj.php">Références & Projets</a></li>
+                        <li><a href="../public/blog.php">Blog</a></li>
+                        <li><a href="../public/contact.php">Contact</a></li>
+                    </ul>
+
+                    <!-- social media icons -->
+                    <ul class="nav navbar-nav social-icons">
+                        <li class="social-media"><a href="https://fr-fr.facebook.com/LaFolleAgence/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li class="social-media"><a href="https://twitter.com/lafolleagence/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li class="social-media"><a href="https://www.instagram.com/lafolleagence/"><i class="fa fa-instagram" aria-hidden="true"></i></span></a></li>
+                        <li class="social-media"><a href="https://plus.google.com/+Lafolleagence"><i class="fa fa-google-plus" aria-hidden="true"></i></span></a></li>
+                        <li class="social-media"><a href="http://www.justine-remaudiere.com/"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div><!-- End collapse navbar-->
+            </div><!-- End row-->
+        </div><!-- End container -->
+    </div><!-- End container fluid-->
+</nav><!-- End navbar-->
