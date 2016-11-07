c876608748f3f215d760d5187e6ca630b072eb2e
Wingelo <cedric.gamrat@gmail.com>
2016-11-07 15:44:45 +0100

    modification

--- src/LaFolleAgenceBundle/Controller/CommentController.php
+++ src/LaFolleAgenceBundle/Controller/CommentController.php
@@ -4,10 +4,6 @@ namespace LaFolleAgenceBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use LaFolleAgenceBundle\Entity\Comment;
 
 /**
